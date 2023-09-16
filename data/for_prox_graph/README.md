4c5b98a61f84532cce644594aa3dc0631b223615
ethanmock <yixin.su@outlook.com>
2023-09-16 10:58:50 +0800

    refine prox graph gen

--- data/for_prox_graph/README.md
+++ data/for_prox_graph/README.md
@@ -1,3 +1,13 @@
-Use KB_entity_type.ipynb to obtain the entity types.
 
-Use prompy_gen.py to generate the prompt for LLMs to align the types between two KGs.
+## Mandatory:
+
+Use KB_entity_type.ipynb to obtain the entity type sets and the meta proximity graph without entity types aligned.
+
+
+## Optional:
+
+Use `python prompy_gen.py` to generate the prompt for LLMs to align the types between two KGs.
+
+Obtain the aligned type pairs from any LLM (e.g., Claude) and store the aligned type pairs in `matched_types.txt`.
+
+Replace the types in the meta proximity graph with the aligned types using `python match_type.py`.
---
+++ data/for_prox_graph/match_type.py
@@ -0,0 +1,22 @@
+import pickle
+align_dict = {}
+with open('matched_types.txt', 'r') as f:
+    for line in f:
+        line = line.strip().split('\t')
+        align_dict[line[0]] = line[1]
+
+meta_prox_graph = 'yago_pred_prox_graph'
+aligned_prox_graph = []
+with open(f"{meta_prox_graph}.txt", 'r') as f:
+    for line in f:
+        line = line.strip().split('\t')
+        if line[0] in align_dict:
+            line[0] = align_dict[line[0]]
+        if line[1] in align_dict:
+            line[1] = align_dict[line[1]]
+        
+        aligned_prox_graph.append(line)
+
+
+pickle.dump(aligned_prox_graph, open(f"{meta_prox_graph}.pickle", 'wb'))
+
